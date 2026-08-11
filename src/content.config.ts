2d762fda26fd40255125362e23924921c4b7b0d2
yamasitas <x5@y31.com>
2026-08-11 10:50:59 +0900

    test4

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			mangaImage: z.optional(image()),
 		}),
 });
 
